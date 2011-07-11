8919bca1921fc4f112ec9efe2a18150825dc7b3e
Michael JasonSmith <mpj17@onlinegroups.net>
2011-07-11 11:09:05 +1000

    Clean up: removing the old navigation links metadata

--- gs/group/messages/topic/interfaces.py
+++ gs/group/messages/topic/interfaces.py
@@ -22,44 +22,6 @@ class IGSAddToTopicFields(IGSPostMessage, IGSStickyTopic):
       description=u'The ID of the most recent post to the topic',
       required=True)
 
-
-class INavLinksContentProvider(Interface):
-    topicTitle = TextLine(title=u"Title of the Topic",
-        description=u'The title of the topic.',
-        required=True)
-                     
-    previousTopic = Field(title=u'Previous Topic',
-        description=u'The previous topic in the group same topic.',
-        required=True)
-
-    nextTopic = Field(title=u'Next Topic',
-        description=u'The next topic in the group same topic.',
-        required=True)
-
-    pageTemplateFileName = ASCIILine(title=u"Page Template File Name",
-        description=u"""The name of the ZPT file
-        that is used to render the post.""",
-        required=False,
-        default='browser/templates/navlinks.pt')
-
-class IGSTopicSummaryContentProvider(Interface):
-    """The Groupserver Topic Summary Content Provider Interface
-      
-      This interface defines the fields that must be set up, normally using
-      TAL, before creating a "GSTopicSummaryContentProvider" instance. 
-      See the latter for an example."""
-    
-    topic = Field(title=u"Topic",
-        description=u"The topic to display",
-        required=True, 
-        readonly=False)
-
-    pageTemplateFileName = ASCIILine(title=u"Page Template File Name",
-        description=u'The name of the ZPT file that is used to '\
-            u'render the post.',
-        required=False,
-        default="browser/templates/summary.pt")
-
 class IGSStickyTopicToggleContentProvider(Interface):
     """A content provider for the sticky-topic toggle"""
     topic = TextLine(title=u"Topic",
