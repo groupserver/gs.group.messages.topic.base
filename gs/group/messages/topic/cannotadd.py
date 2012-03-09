7847de2246739d06d5c2f70643825f8185b91ad3
Michael JasonSmith <mpj17@onlinegroups.net>
2012-03-09 15:41:59 +1100

    Moving most of the Can Post logic to gs.group.member.canpost

--- gs/group/messages/topic/cannotadd.py
+++ gs/group/messages/topic/cannotadd.py
@@ -5,23 +5,5 @@ from gs.group.base.viewlet import GroupViewlet
 # TODO: Move to gs.group.member.canpost
 
 class CannotAddToTopic(GroupViewlet):
-    def __init__(self, messages, request, view, manager):
-        GroupViewlet.__init__(self, messages, request, view, manager)
-
-    @Lazy
-    def show(self):
-        postingInfo = self.view.userPostingInfo
-        assert postingInfo
-        retval = not(postingInfo.canPost)
-        return retval
-
-    @Lazy
-    def statusNum(self):
-        retval = self.view.userPostingInfo.statusNum
-        return retval
-        
-    @Lazy
-    def status(self):
-        retval = self.view.userPostingInfo.status
-        return retval
+    '''The suppplier of *ahem* the can-post viewlet manager'''
 
