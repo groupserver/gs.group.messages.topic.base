7c0a5a4c15f63ac545c720a3f575bac4a1a83e8a
Michael JasonSmith <mpj17@onlinegroups.net>
2012-03-09 15:45:08 +1100

    Further simplification of the Can Post code.

--- gs/group/messages/topic/cannotadd.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-from zope.cachedescriptors.property import Lazy
-from gs.group.base.viewlet import GroupViewlet
-
-# TODO: Move to gs.group.member.canpost
-
-class CannotAddToTopic(GroupViewlet):
-    '''The suppplier of *ahem* the can-post viewlet manager'''
-
