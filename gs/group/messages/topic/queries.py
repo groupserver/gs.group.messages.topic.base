ef34d445a38e810d2dac7b126ef1f70208e15ab1
mpj17@onlinegroups.net <mpj17@onlinegroups.net>
2012-06-19 18:59:02 +1200

    Updating the queries to gs.database.

--- gs/group/messages/topic/queries.py
+++ gs/group/messages/topic/queries.py
@@ -2,23 +2,25 @@
 import sqlalchemy as sa
 from datetime import datetime
 from pytz import UTC
+from gs.database import getSession, getTable
 
 class TopicQuery(object):
     def __init__(self, context, da):
         self.context = context
         
-        self.topicTable = da.createTable('topic')
-        self.postTable = da.createTable('post')
+        self.topicTable = getTable('topic')
+        self.postTable = getTable('post')
 
     def topic_hidden(self, postId):
         s1 = sa.select([self.postTable.c.topic_id])
         s1.append_whereclause(self.postTable.c.post_id == postId)
-        s1.salias('ss')
+        s1.alias('ss')
 
         s2 = sa.select(self.topicTable.hidden)
         s2.append_whereclause(ss.c.topic_id)
         
-        r = s2.execute()
+        session = getSession()
+        r = session.execute(s2)
         x = r.fetchone()
         retval = bool(x['hidden'])
         return retval
@@ -26,20 +28,21 @@ class TopicQuery(object):
     def topic_sticky(self, topicId):
         s = sa.select([self.topicTable.c.sticky])
         s.append_whereclause(self.topicTable.c.topic_id == topicId)
-        
-        r = s.execute()
+        session = getSession()
+        r = session.execute(s)
         
         x = r.fetchone()
         retval = bool(x['sticky'])
         return retval
 
     def set_sticky(self, topicId, sticky):
+        session = getSession()
         tt = self.topicTable
+        u = tt.update(tt.c.topic_id == topicId)
         if sticky:
             v = datetime.utcnow()
         else:
             v = None
-        u = tt.update(tt.c.topic_id == topicId, 
-                        values = {'sticky': v})
-        u.execute()
+        d = {'sticky': v}
+        session.execute(u, params=d)
 
