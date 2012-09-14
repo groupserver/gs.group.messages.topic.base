3cc871d76255c1480852abe7a188c55a07c7053f
Michael JasonSmith <mpj17@onlinegroups.net>
2012-09-14 15:52:14 +1000

    Adding the fts_vectors column to the topic table.

--- gs/group/messages/topic/sql/01-topic.sql
+++ gs/group/messages/topic/sql/01-topic.sql
@@ -26,3 +26,45 @@ CREATE TABLE topic (
 
 CREATE INDEX GROUP_ID_SITE_ID_IDX ON topic USING BTREE (group_id, site_id);
 -- ALTER TABLE topic ADD column hidden TIMESTAMP WITH TIME ZONE;
+CREATE INDEX topic_fts_vectors ON topic USING gin(fts_vectors);
+
+-- Installs up to and including GS 12.05 will need to populate the
+--   full-text search column of the topic table.
+-- CREATE OR REPLACE FUNCTION topic_fts_vectors_populate () RETURNS void AS $$
+-- DECLARE
+--    trecord RECORD;
+--  BEGIN
+--    FOR trecord IN SELECT * FROM topic WHERE tsv IS NULL LOOP
+--      UPDATE topic 
+--        SET tsv=to_tsvector('english', ss.agg_body) 
+--        FROM (SELECT string_agg(body, '') AS agg_body
+--                FROM post, topic 
+--                WHERE post.topic_id = trecord.topic_id
+--                      AND post.topic_id = topic.topic_id
+--                      AND topic.fts_vectors IS NULL) AS ss 
+--        WHERE topic_id=trecord.topic_id;
+--    END LOOP;
+--  END;
+-- $$ LANGUAGE 'plpgsql';
+
+-- The topic fts_vectors column contains the full-text retrieval
+--   information for all the posts in the topic. It is updated after a
+--   *post* has been added to the database. In effect it concatenates all
+--   the bodies together (string_agg(post.body, ' ')) and turns them into a
+--   text-search vector (to_tsvector).
+CREATE OR REPLACE FUNCTION topic_fts_vectors_update ()
+  RETURNS TRIGGER
+  AS $$
+    BEGIN
+      UPDATE topic 
+        SET topic.fts_vectors = to_tsvector('english', ss.agg_body) 
+        FROM (SELECT string_agg(post.body, ' ') AS agg_body
+              FROM post
+              WHERE post.topic_id = NEW.topic_id) AS ss 
+        WHERE topic.topic_id = NEW.topic_id;
+      RETURN NULL;
+    END;  
+  $$ LANGUAGE plpgsql;
+CREATE TRIGGER topic_fts_vectors_update_trigger 
+  AFTER INSERT OR UPDATE ON post -- Yes, AFTER; yes, post.
+  EXECUTE PROCEDURE topic_fts_vectors_update ();
