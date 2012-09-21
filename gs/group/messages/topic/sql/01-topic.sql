e9be5b26f62391b64382f277d3c792579f4443bb
Michael JasonSmith <mpj17@onlinegroups.net>
2012-09-21 13:16:05 +1000

    Moving the topic_body and the topic_fts_update functions into the 01-topic.sql file.

--- gs/group/messages/topic/sql/01-topic.sql
+++ gs/group/messages/topic/sql/01-topic.sql
@@ -30,5 +30,51 @@ CREATE TABLE topic (
 CREATE INDEX GROUP_ID_SITE_ID_IDX ON topic USING BTREE (group_id, site_id);
 CREATE INDEX topic_fts_vectors ON topic USING gin(fts_vectors);
 
--- See 03-keywords.sql in this directory for the trigger that updates the
--- fts_vectors and keywords columns.
+
+-- Get the body of all the posts in a topic as a single string
+--
+-- ARGUMENTS
+--
+--   topic_id  The identifier for the topic.
+--
+-- RETURNS
+--
+--   A single string that is made up from of all bodies of all the posts in
+--   the topic. It is limited to 1048575 characters, as that is the maximum
+--   length for a vector.
+CREATE OR REPLACE FUNCTION topic_body (topic_id TEXT)
+  RETURNS TEXT AS $$
+  DECLARE
+      topic_text TEXT;
+      subject TEXT;
+      retval TEXT;
+  BEGIN
+    SELECT string_agg(post.body, ' ') INTO topic_text
+      FROM post 
+      WHERE post.topic_id = topic_body.topic_id
+        AND post.hidden IS NULL;
+    SELECT COALESCE(original_subject, '') INTO subject
+      FROM topic WHERE topic.topic_id = topic_body.topic_id;
+    retval := left(subject || ' ' || topic_text, 1048575); -- Max for a vector
+    RETURN retval;
+  END;
+$$ LANGUAGE 'plpgsql';
+
+
+-- The trigger for updating the full-text retrieval information, and the
+-- keywords, in a topic. The topic fts_vectors column contains the
+-- full-text retrieval information for all the posts in the topic. It is
+-- updated after a *post* has been added to the database.
+CREATE OR REPLACE FUNCTION topic_fts_update ()
+  RETURNS TRIGGER AS $$
+    DECLARE
+      topic_text TEXT;
+    BEGIN
+      topic_text := topic_body(NEW.topic_id);
+      NEW.fts_vectors := to_tsvector('english', topic_text);
+      RETURN NEW;
+    END;  
+$$ LANGUAGE 'plpgsql';
+CREATE TRIGGER topic_update_trigger_01
+  BEFORE INSERT OR UPDATE ON topic
+  FOR EACH ROW EXECUTE PROCEDURE topic_fts_update ();
