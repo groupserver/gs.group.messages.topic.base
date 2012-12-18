bd10a74de6cba4c3bdaa0078cb1f7c96414a2ade
Michael JasonSmith <mpj17@onlinegroups.net>
2012-12-18 17:47:30 +1100

    Adding a Tasks viewlet

---
+++ gs/group/messages/topic/tasks.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from zope.cachedescriptors.property import Lazy
+from gs.group.base.viewlet import GroupViewlet
+
+
+class TasksViewlet(GroupViewlet):
+    def __init__(self, group, request, view, manager):
+        super(TasksViewlet, self).__init__(group, request, view, manager)
+
+    @property
+    def topic(self):
+        retval = getattr(self.view, 'topic')
+        return retval
+
+    @Lazy
+    def previousTopic(self):
+        # TODO: Figure out why getattr is needed
+        retval = getattr(self.view, 'previousTopic')
+        return retval
+
+    @Lazy
+    def nextTopic(self):
+        retval = getattr(self.view, 'nextTopic')
+        return retval
