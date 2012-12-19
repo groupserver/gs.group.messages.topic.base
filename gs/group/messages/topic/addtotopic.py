7f636c6214950a133468aef83e3a953c3be9c295
Michael JasonSmith <mpj17@onlinegroups.net>
2012-12-19 14:55:16 +1100

    Getting the privacy summary to behave.

--- gs/group/messages/topic/addtotopic.py
+++ gs/group/messages/topic/addtotopic.py
@@ -1,11 +1,12 @@
 # coding=utf-8
 from zope.cachedescriptors.property import Lazy
 from gs.group.base.viewlet import GroupViewlet
-
+from gs.group.messages.privacy import MessagesPrivacy
 # TODO: Move all post-creation code to gs.group.messages.add.base
 # TODO: Convert to z3c.form.
 # TODO: Make this a sub-form.
 #   http://pypi.python.org/pypi/z3c.form#sub-forms
+
 
 class AddToTopic(GroupViewlet):
     def __init__(self, messages, request, view, manager):
@@ -43,3 +44,7 @@ class AddToTopic(GroupViewlet):
         retval = self.view.availableActions
         return retval
 
+    @Lazy
+    def privacy(self):
+        retval = MessagesPrivacy(self.context)
+        return retval
