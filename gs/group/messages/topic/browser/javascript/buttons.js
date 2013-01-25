37a528e4ead792621e76c7e35b8ca9b353c2b7b8
Michael JasonSmith <mpj17@onlinegroups.net>
2013-01-25 16:20:38 +1100

    First cut at getting the sticky-topic toggle to at least *look* ok with the new Boostrap buttons

--- gs/group/messages/topic/browser/javascript/buttons.js
+++ gs/group/messages/topic/browser/javascript/buttons.js
@@ -3,19 +3,9 @@ jQuery.noConflict();
 var GSTopicButtons = function () {
     // Private variables
     var NAVLINKS = '.gs-group-messages-topic-navlinks';
-    var prev = {icons: { primary: "ui-icon-carat-1-w" }, text: true};
-    var up = {icons: { primary: "ui-icon-arrowthick-1-n" }, text: false};
-    var next = {icons: { secondary: "ui-icon-carat-1-e" }, text: true};
 
     return {
         init: function() {
-            jQuery(NAVLINKS + ' .prev').button(prev);
-            jQuery(NAVLINKS + ' span.prev').button("disable");
-            jQuery(NAVLINKS + ' .up').button(up);
-            jQuery(NAVLINKS + ' .next').button(next);
-            jQuery(NAVLINKS + ' span.next').button("disable");
-            
-            jQuery('#form\\.actions\\.post').button();
         }//init
     };
 }();
