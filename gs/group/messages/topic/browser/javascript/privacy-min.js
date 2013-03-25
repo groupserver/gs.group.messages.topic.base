490dc68658b3ed1188b7038a9dc2688c8d778781
Michael JasonSmith <mpj17@onlinegroups.net>
2013-03-25 11:29:48 +1100

    Adding minified versions of the Sticky Toggle and Privacy JavaScript resources.

---
+++ gs/group/messages/topic/browser/javascript/privacy-min.js
@@ -0,0 +1,3 @@
+jQuery.noConflict();jQuery(window).load(function(){var a=null,b=null;a=jQuery("#gs-group-messages-topic-add-privacy").html();
+b={animation:true,html:true,placement:"top",trigger:"click",content:a};jQuery("#gs-group-messages-topic-add-privacy-summary").popover(b)
+});
