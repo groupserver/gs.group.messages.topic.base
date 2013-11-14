1a6062ce070eca5c72fafbb0e645c9a2425befb0
Michael JasonSmith <mpj17@onlinegroups.net>
2013-11-14 10:06:28 -0800

    Updating the egg deps.

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2013 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
 import os
 from setuptools import setup, find_packages
 from version import get_version
@@ -29,10 +42,12 @@ setup(name='gs.group.messages.topic',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['gs', 'gs.group', 'gs.group.messages'],
     include_package_data=True,
-    zip_safe=True,
+    zip_safe=False,
     install_requires=[
         'setuptools',
         'SQLAlchemy',
+        'zope.browserpage',
+        'zope.browserresource',
         'zope.cachedescriptors',
         'zope.component',
         'zope.contentprovider',
@@ -43,6 +58,8 @@ setup(name='gs.group.messages.topic',
         'zope.schema',
         'zope.security',
         'zope.sqlalchemy',
+        'zope.tal',
+        'zope.tales',
         'zope.viewlet',
         'gs.content.js.multifile',
         'gs.content.js.sharebox',
