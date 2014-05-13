c42d51cb114b11be5c4a82494b234e3f2cc04367
Michael JasonSmith <mpj17@onlinegroups.net>
2014-05-13 15:02:29 -0700

    Updating the egg metadata

--- setup.py
+++ setup.py
@@ -12,17 +12,22 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
+import codecs
 import os
 from setuptools import setup, find_packages
 from version import get_version
 
 version = get_version()
 
+with codecs.open('README.txt', encoding='utf-8') as f:
+    long_description = f.read()
+with codecs.open(os.path.join("docs", "HISTORY.txt"), encoding='utf-8') as f:
+    long_description += '\n' + f.read()
+
 setup(name='gs.group.messages.topic',
     version=version,
     description="Topics in a GroupServer Group",
-    long_description=open("README.txt").read() + "\n" +
-                      open(os.path.join("docs", "HISTORY.txt")).read(),
+    long_description=long_description,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         "Environment :: Web Environment",
@@ -32,12 +37,17 @@ setup(name='gs.group.messages.topic',
         "Natural Language :: English",
         "Operating System :: POSIX :: Linux"
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: JavaScript",
+        "Programming Language :: PL/SQL",
+        "Topic :: Communications :: Email",
+        "Topic :: Communications :: Email :: Mailing List Servers",
         "Topic :: Software Development :: Libraries :: Python Modules",
       ],
-    keywords='groupserver message post topic',
+    keywords='groupserver, message, post, topic',
     author='Michael JasonSmith',
     author_email='mpj17@onlinegroups.net',
-    url='http://groupserver.org/',
+    url='https://source.iopen.net/groupserver/gs.group.messages.topic/',
     license='ZPL 2.1',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['gs', 'gs.group', 'gs.group.messages'],
