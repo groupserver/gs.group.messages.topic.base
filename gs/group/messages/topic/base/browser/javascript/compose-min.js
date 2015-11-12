2b6443ffd9925c620fe3b78bec88b08a0dcadeff
Michael JasonSmith <mpj17@onlinegroups.net>
2015-11-12 14:30:41 -0800

    gjslint, and re-minifying

--- gs/group/messages/topic/base/browser/javascript/compose-min.js
+++ gs/group/messages/topic/base/browser/javascript/compose-min.js
@@ -1,10 +1,3 @@
-"use strict";jQuery.noConflict();function GSFileUpload(e,d,b){var a='<abbr class="muted" title="Remove this file from the list of files">(remove)</abbr>';
-function c(j){var k=null,l="",g="",h=null,f=null;k=jQuery(".MultiFile-applied");for(h=0;
-h<k.length;h++){f=jQuery(k[h]);l=f.attr("id");g=l+"."+h;f.attr("id",g).attr("name",g)
-}return true}return{init:function(){var f={list:b,STRING:{remove:a}};jQuery(d).MultiFile(f);
-jQuery(e).submit(c)}}}function gs_group_messages_topic_multifile_init(){var a=null;
-a=GSFileUpload(".gs-group-messages-topic-add","#form\\.uploadedFile",".gs-group-messages-topic-add-file-files-list");
-a.init()}jQuery(window).load(function(){var a=null,b=null;if(jQuery(".gs-group-messages-topic-add").length!=0){a=jQuery("#gs-group-messages-topic-add-privacy").html();
-b={animation:true,html:true,placement:"top",trigger:"click",content:a};jQuery("#gs-group-messages-topic-add-privacy-summary").popover(b);
-gsJsLoader.with_module("/++resource++multiple_file_upload-1.48/jquery.MultiFile.js",gs_group_messages_topic_multifile_init)
-}});
+'use strict';jQuery.noConflict();function GSFileUpload(a,e,f){function g(a){a=null;var b="",b="",c=null,d=null;a=jQuery(".MultiFile-applied");for(c=0;c<a.length;c++)d=jQuery(a[c]),b=d.attr("id"),b=b+"."+c,d.attr("id",b).attr("name",b);return!0}return{init:function(){var h={list:f,STRING:{remove:'<abbr class="muted" title="Remove this file from the list of files">(remove)</abbr>'}};jQuery(e).MultiFile(h);jQuery(a).submit(g)}}}
+function gs_group_messages_topic_multifile_init(){var a=null,a=GSFileUpload(".gs-group-messages-topic-add","#form\\.uploadedFile",".gs-group-messages-topic-add-file-files-list");a.init()}
+jQuery(window).load(function(){var a=null,a=null;0!=jQuery(".gs-group-messages-topic-add").length&&(a=jQuery("#gs-group-messages-topic-add-privacy").html(),a={animation:!0,html:!0,placement:"top",trigger:"click",content:a},jQuery("#gs-group-messages-topic-add-privacy-summary").popover(a),gsJsLoader.with_module("/++resource++multiple_file_upload-1.48/jquery.MultiFile.js",gs_group_messages_topic_multifile_init))});
--- gs/group/messages/topic/base/browser/javascript/compose.js
+++ gs/group/messages/topic/base/browser/javascript/compose.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 // GroupServer JavaScript for composing a post
 //
 // Copyright © 2013, 2014 OnlineGroups.net and Contributors.
@@ -18,20 +18,20 @@ function GSFileUpload(formId, widgetId, listId) {
                  'of files">(remove)</abbr>';
     // Private methods
     function renameFileInputs(event) {
-        var multiFiles=null, oldId='', newId='', i=null, input=null;
+        var multiFiles = null, oldId = '', newId = '', i = null, input = null;
         multiFiles = jQuery('.MultiFile-applied');
-        for ( i=0; i < multiFiles.length; i++ ) {
+        for (i = 0; i < multiFiles.length; i++) {
             input = jQuery(multiFiles[i]);
             oldId = input.attr('id');
             newId = oldId + '.' + i;
             input.attr('id', newId).attr('name', newId);
         }
         return true;
-    }
+    }//renameFileInputs
 
     // Public methods and properties
     return {
-        init: function () {
+        init: function() {
             var options = {
                 list: listId,
                 STRING: {remove: remove}
@@ -42,25 +42,27 @@ function GSFileUpload(formId, widgetId, listId) {
     };
 } // GSFileUpload
 
-function gs_group_messages_topic_multifile_init () {
-    var uploader=null;
-    uploader = GSFileUpload('.gs-group-messages-topic-add', 
-                            '#form\\.uploadedFile',
-                            '.gs-group-messages-topic-add-file-files-list');
+function gs_group_messages_topic_multifile_init() {
+    var uploader = null;
+    uploader = GSFileUpload(
+        '.gs-group-messages-topic-add',
+        '#form\\.uploadedFile',
+        '.gs-group-messages-topic-add-file-files-list');
     uploader.init();
-}
+}//gs_group_messages_topic_multifile_init
 
-jQuery(window).load( function () {
-    var h=null, d=null;
- 
+jQuery(window).load(function() {
+    var h = null, d = null;
+
    if (jQuery('.gs-group-messages-topic-add').length != 0) {
        h = jQuery('#gs-group-messages-topic-add-privacy').html();
-       d = {animation: true, html: true, placement: 'top', trigger: 'click', 
+       d = {animation: true, html: true, placement: 'top', trigger: 'click',
             content: h};
        jQuery('#gs-group-messages-topic-add-privacy-summary').popover(d);
 
        // Init the file uploader
-       gsJsLoader.with_module('/++resource++multiple_file_upload-1.48/jquery.MultiFile.js', 
-                              gs_group_messages_topic_multifile_init);
+       gsJsLoader.with_module(
+           '/++resource++multiple_file_upload-1.48/jquery.MultiFile.js',
+           gs_group_messages_topic_multifile_init);
    }
 });
